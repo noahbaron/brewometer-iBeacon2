317e5a8d7e0f9c3b9baf6e1603e19fe462b8355e
Noah Baron <noahbaron@gmail.com>
2015-12-08 08:24:36 -0800

    copied new app.js from evothings

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ var app = (function()
 	// Specify your beacon 128bit UUIDs here.
 	var regions =
 	[
-		{uuid:'A495BB10-C5B1-4B44-B512-1370F02D74DE'},
-		{uuid:'A495BB20-C5B1-4B44-B512-1370F02D74DE'},
-		{uuid:'A495BB30-C5B1-4B44-B512-1370F02D74DE'},
-		{uuid:'A495BB40-C5B1-4B44-B512-1370F02D74DE'}
-
+		// Estimote Beacon factory UUID.
+		{uuid:'B9407F30-F5F8-466E-AFF9-25556B57FE6D'},
+		// Sample UUIDs for beacons in our lab.
+		{uuid:'F7826DA6-4FA2-4E98-8024-BC5B71E0893E'},
+		{uuid:'8DEEFBB9-F738-4297-8040-96668BB44281'},
+		{uuid:'A0B13730-3A9A-11E3-AA6E-0800200C9A66'},
+		{uuid:'E20A39F4-73F5-4BC4-A12F-17D1AD07A961'},
+		{uuid:'A4950001-C5B1-4B44-B512-1370F02D74DE'},
+		{uuid:'585CDE93-1B01-42CC-9A13-25009BEDC65E'},	// Dialog Semiconductor.
 	];
 
 	// Dictionary of beacons.
@@ -21,7 +25,10 @@ var app = (function()
 
 	app.initialize = function()
 	{
-		document.addEventListener('deviceready', onDeviceReady, false);
+		document.addEventListener(
+			'deviceready',
+			function() { evothings.scriptsLoaded(onDeviceReady) },
+			false);
 	};
 
 	function onDeviceReady()
@@ -51,9 +58,8 @@ var app = (function()
 				// Insert beacon into table of found beacons.
 				var beacon = pluginResult.beacons[i];
 				beacon.timeStamp = Date.now();
-				var key = beacon.uuid; //+ ':' + beacon.major + ':' + beacon.minor;
+				var key = beacon.uuid + ':' + beacon.major + ':' + beacon.minor;
 				beacons[key] = beacon;
-
 			}
 		};
 
@@ -97,6 +103,7 @@ var app = (function()
 				.done();
 		}
 	}
+
 	function displayBeaconList()
 	{
 		// Clear beacon list.
@@ -114,74 +121,22 @@ var app = (function()
 				var rssiWidth = 1; // Used when RSSI is zero or greater.
 				if (beacon.rssi < -100) { rssiWidth = 100; }
 				else if (beacon.rssi < 0) { rssiWidth = 100 + beacon.rssi; }
-				
-				var date = new Date(beacon.timeStamp);
 
-				// Map the temperature to a width in percent for the indicator.
-				var tempWidth = beacon.major/185*100;
-
-
-				// Convert SG units and map the specific gravity to a width in percent for the indicator.
-				var sgStandardUnits = beacon.minor/1000;
-				var sgFix3 = sgStandardUnits.toFixed(3);
-				
-                var sgWidth = 1; // Used when SG is less than 990.
-				if (beacon.minor > 1120) { sgWidth = 100; }
-				else if (beacon.minor < 990) { sgWidth = 1; }
-				else { sgWidth = (beacon.minor-990)/130*100; }
-				
-				//corrected SG calc (divide by 7 to compensate for expansion of brewtainer)
-				var sgCorrection = (-0.000000006130*beacon.major*beacon.major*beacon.major + 0.000002934888*beacon.major*beacon.major - 0.000199630555*beacon.major + 0.002825186384) / 7;
-				var sgCorrection3 = sgCorrection.toFixed(3);
-				var sgCorrected = Number(sgCorrection3) + Number(sgFix3);
-				//var sgCorrected3 = sgCorrected.toFixed(3);
-
-				var sgCorrWidth = sgCorrection3*100+1;
-
-				var brewUUID = beacon.uuid;
-				var brewArray = 
-				["A495BB10-C5B1-4B44-B512-1370F02D74DE",
-				 "a495bb10-c5b1-4b44-b512-1370f02d74de",
-				 "A495BB20-C5B1-4B44-B512-1370F02D74DE",
-				 "a495bb20-c5b1-4b44-b512-1370f02d74de",
-				 "A495BB30-C5B1-4B44-B512-1370F02D74DE",
-				 "a495bb30-c5b1-4b44-b512-1370f02d74de",
-				 "A495BB40-C5B1-4B44-B512-1370F02D74DE",
-				 "a495bb40-c5b1-4b44-b512-1370f02d74de"];
-				 var brewVariety = ["RED","RED", "GREEN","GREEN", "BLACK","BLACK", "PURPLE", "PURPLE"];
-
-				 for(var i=0; i<brewArray.length; i++) {
-                 if (brewUUID == brewArray[i]){
-                 var brewVarietyValue = brewVariety[i];
-				}
-			}
 				// Create tag to display beacon data.
 				var element = $(
 					'<li>'
-					//+	'<strong>UUID: ' + beacon.uuid + '</strong><br />'
-					+	'<strong>BREWOMETER | ' + brewVarietyValue + '</strong><br />'//<br />'
-					+ 	'<div style="background:' + brewVarietyValue + ';height:40px;width:'
-					+ 		100 + '%;"></div>'
-					+	'Specific Gravity (cal. @68°F):<br /><h1>' + sgFix3 + '</h1>'
-					+ 	'<div style="background:' + brewVarietyValue + ';height:10px;width:'
-					+ 		sgWidth + '%;"></div>'
-					+	'Temperature:<br /><h1>' + beacon.major + '°F</h1>'
-					+ 	'<div style="background:' + brewVarietyValue + ';height:10px;width:'
-					+ 		tempWidth + '%;"></div>'
-					+	'Temperature Correction:<br /><h1>' + sgCorrection3 + '</h1>'
-					+ 	'<div style="background:' + brewVarietyValue + ';height:10px;width:'
-					+ 		sgCorrWidth + '%;"></div>'
-		            +	'Signal Strength:<br /><h1>' + rssiWidth + '%</h1>'
-					+ 	'<div style="background:' + brewVarietyValue + ';height:10px;width:'
+					+	'<strong>UUID: ' + beacon.uuid + '</strong><br />'
+					+	'Major: ' + beacon.major + '<br />'
+					+	'Minor: ' + beacon.minor + '<br />'
+					+	'Proximity: ' + beacon.proximity + '<br />'
+					+	'RSSI: ' + beacon.rssi + '<br />'
+					+ 	'<div style="background:rgb(255,128,64);height:20px;width:'
 					+ 		rssiWidth + '%;"></div>'
-					+	'<h4>' + date + '</h4>'
-					//+	'Proximity: ' + beacon.proximity + '<br />'
 					+ '</li>'
 				);
 
 				$('#warning').remove();
-				$('#found-beacons').prepend(element);
-
+				$('#found-beacons').append(element);
 			}
 		});
 	}
